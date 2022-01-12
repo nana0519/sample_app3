9872efa26864b9151b87940e6fe16c8d05784e80
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-01-12 06:41:13 +0000

    [Update]3-5

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as:'list'
+  get 'lists/:id/edit' => 'lists#edit', as:'list_edit'
+  patch 'lists/:id' => 'lists#update', as:'list_update'
+  get '/top' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
