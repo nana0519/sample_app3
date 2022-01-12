4f4a6a6075f1d37766b9af4643318122a4d9d6e4
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-01-12 09:27:57 +0000

    [Finish]3-11

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'lists/:id/edit' => 'lists#edit', as:'list_edit'
   patch 'lists/:id' => 'lists#update', as:'list_update'
   get '/top' => 'homes#top'
+  delete 'lists/:id' => 'lists#destroy', as:'list_destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
